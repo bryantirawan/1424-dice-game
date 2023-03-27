df9995d0b8da2292689d2baa2695ec9c9d4e6071
bryantirawan <bryantirawan@gmail.com>
2023-03-27 09:31:19 -0700

    Initial Commit

---
+++ 1424 Swift UI/_424_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _424_Swift_UIApp.swift
+//  1424 Swift UI
+//
+//  Created by Bryant Irawan on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct _424_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
